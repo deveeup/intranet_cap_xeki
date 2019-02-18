815d1cad2e178ed6f409f40088d163e5ca61731f
David R <jossedaviid1@gmail.com>
2019-02-18 12:19:29 -0500

    login and initial body

--- core/url.php
+++ core/url.php
@@ -1,15 +1,16 @@
 <?php
-// home route
-\xeki\routes::get('', 'login');
 
-\xeki\routes::get('url', function($vars){
-  $title = "title for seo";
-  $description =  "description for seo";
-  \xeki\html_manager::set_seo($title,$description,true);
+  \xeki\routes::get('iniciar-sesion', 'login');
+  \xeki\routes::get('', 'home');
 
-  $items_to_print=array();
-  \xeki\html_manager::render('name.html',$items_to_print);
-});
+  \xeki\routes::get('url', function($vars){
+    $title = "title for seo";
+    $description =  "description for seo";
+    \xeki\html_manager::set_seo($title,$description,true);
+
+    $items_to_print=array();
+    \xeki\html_manager::render('name.html',$items_to_print);
+  });
 
 
 // urls with slugs
