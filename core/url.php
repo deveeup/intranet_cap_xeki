cbcc2e9f7bc73de323a097e81ce74a5d17cdc5c5
David R <jossedaviid1@gmail.com>
2019-03-06 16:05:53 -0500

    add auth

--- core/url.php
+++ core/url.php
@@ -19,9 +19,16 @@
 
 // Static pages
   \xeki\routes::any('trabaja-con-nosotros', function($vars){
-  $title = "title for seo";
-  $description =  "description for seo";
-  \xeki\html_manager::set_seo($title,$description,true);
-  $items_to_print = array();
-  \xeki\html_manager::render('path/name.html', $items_to_print);
+    $title = "title for seo";
+    $description =  "description for seo";
+    \xeki\html_manager::set_seo($title,$description,true);
+    $items_to_print = array();
+    \xeki\html_manager::render('path/name.html', $items_to_print);
+  });
+
+//logout 
+  \xeki\routes::any('logout', function(){
+    $auth = \xeki\module_manager::import_module('auth');
+    $auth->logout();
+    \xeki\core::redirect('');
   });
