d30f90642745d0fb4d0d451bfc172b7f63f99846
David R <jossedaviid1@gmail.com>
2019-03-14 12:11:08 -0500

    add data in main.php

--- core/main.php
+++ core/main.php
@@ -1,11 +1,19 @@
 <?php
 
-
+#import modules
 $auth = \xeki\module_manager::import_module('auth');
+$csrf = \xeki\module_manager::import_module('csrf');
 
+#global data
+\xeki\html_manager::add_extra_data("cap","https://capillasdelafe.com/funeraria");
+\xeki\html_manager::add_extra_data("coor","https://coorserpark.com/");
+\xeki\html_manager::add_extra_data("alt","Intranet Funeraria Capillas de La Fe -");
+\xeki\html_manager::add_extra_data("csrf", $csrf->get_token_html()); #token csrf
+
+#validate logged
 $status = $auth->is_logged();
+
 if($status){
-    // is logged
     $user = $auth->get_user();
     $user_info = $user->get_info();
 
@@ -14,8 +22,6 @@ if($status){
     \xeki\html_manager::add_extra_data("last_name", $user->get("last_name"));
     \xeki\html_manager::add_extra_data("email", $user->get("email"));
     \xeki\html_manager::add_extra_data("first_name", $user->get("first_name"));
+
 }
 
-$csrf = \xeki\module_manager::import_module('csrf');
-
-\xeki\html_manager::add_extra_data("csrf", $csrf->get_token_html());
