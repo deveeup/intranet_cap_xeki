857a646bb0c631f8c68f2e55131d845a6b35e2e5
David R <jossedaviid1@gmail.com>
2019-04-04 10:14:26 -0500

    create tables in db

--- core/controllers/home.php
+++ core/controllers/home.php
@@ -6,21 +6,27 @@ $auth = \xeki\module_manager::import_module('auth');
 
 #validate logged
 if(!$auth->is_logged()){
-    \xeki\core::redirect('');
+	\xeki\core::redirect('');
 } else {
-    #info seo
-    $title = "Intranet";
-    $description = "Lorem...";
-    $keyworkds= "Funeraria, Coorserpark, Capillas de La Fe, capillas la fe, obituarios, sedes";
-    \xeki\html_manager::set_seo($title,$description,false);
-    
-    #info user
-    $user = $auth->get_user(); 
-    $data['user'] = $user->get_info();
-    
-    #sending data to view
-    $items_to_print=array();
-    $items_to_print['user']= $data['user'];
-    
-    \xeki\html_manager::render('index.html',$items_to_print);
+	#info seo
+	$title = "Intranet";
+	$description = "Lorem...";
+	$keyworkds= "Funeraria, Coorserpark, Capillas de La Fe, capillas la fe, obituarios, sedes";
+	\xeki\html_manager::set_seo($title,$description,false);
+
+	#sql
+	$query = "SELECT * FROM transversal_process order by order_by asc";
+	$transversal_process = $sql->query($query);
+
+	#info user
+	$user = $auth->get_user(); 
+	$data['user'] = $user->get_info();
+	
+	#sending data to view
+	$items_to_print = array();
+	$items_to_print['user'] = $data['user'];
+	$items_to_print['transversal_process'] = $transversal_process;
+
+	
+	\xeki\html_manager::render('index.html',$items_to_print);
 }
--- core/main.php
+++ core/main.php
@@ -3,6 +3,7 @@
 #import modules
 $auth = \xeki\module_manager::import_module('auth');
 $csrf = \xeki\module_manager::import_module('csrf');
+$sql=\xeki\module_manager::import_module("db-sql");
 
 #global data
 \xeki\html_manager::add_extra_data("cap","https://capillasdelafe.com/funeraria");
@@ -13,15 +14,24 @@ $csrf = \xeki\module_manager::import_module('csrf');
 #validate logged
 $status = $auth->is_logged();
 
+
+
 if($status){
-    $user = $auth->get_user();
-    $user_info = $user->get_info();
+	$query = "SELECT * FROM transversal_process order by order_by asc";
+	$transversal_process = $sql->query($query);
 
-    \xeki\html_manager::add_extra_data("auth_user_info", $user_info);    
+	$items_to_global_print = array();
+	$items_to_global_print['transversal_process'] = $transversal_process;
+	
+	$user = $auth->get_user();
+	$user_info = $user->get_info();
 
-    \xeki\html_manager::add_extra_data("last_name", $user->get("last_name"));
-    \xeki\html_manager::add_extra_data("email", $user->get("email"));
-    \xeki\html_manager::add_extra_data("first_name", $user->get("first_name"));
+	\xeki\html_manager::add_extra_data("auth_user_info", $user_info);    
+
+	\xeki\html_manager::add_extra_data("last_name", $user->get("last_name"));
+	\xeki\html_manager::add_extra_data("email", $user->get("email"));
+	\xeki\html_manager::add_extra_data("first_name", $user->get("first_name"));
+	\xeki\html_manager::add_extra_data("transversal_process", $items_to_global_print);
 
 }
 
