14fec3bf12f8358ed8964247eebeec44296d26bd
David R <jossedaviid1@gmail.com>
2019-04-29 16:57:00 -0500

    update list users

--- core/controllers/group_view.php
+++ core/controllers/group_view.php
@@ -26,7 +26,7 @@ if(!$auth->is_logged()){
 		$group_info = $sql->query($queryOne);
 		
 		#relationship user & group (users in group)
-		$queryTwo = "SELECT * FROM auth_user_group WHERE group_ref = '$id_group'";
+		$queryTwo = "SELECT * FROM auth_user_group WHERE group_ref = '$id_group' ";
 		$group_user = $sql->query($queryTwo);
 
 		#send data users
@@ -36,28 +36,28 @@ if(!$auth->is_logged()){
 			$queryThree = "SELECT * FROM auth_user WHERE auth_user.id = '$group[user_ref]' ";
 			$array_user = $sql->query($queryThree);
 			$info_users = $array_user[0];
-			$info_users[groups] = array();
+			$info_users[permission] = array();
 
-			// d($info_users);
-			
 			foreach ($array_user as $user){
-				$queryFour = "SELECT * FROM auth_user_permission WHERE auth_user_permission.user_ref = '$user[id]'  ";
+				$queryFour = "SELECT * FROM auth_user_permission WHERE auth_user_permission.user_ref = '$user[id]' AND auth_user_permission.group_ref = '$id_group' ";
 				$response_auth_user_permission = $sql->query($queryFour);
-				// d($response_auth_user_permission);
 				
 				foreach ($response_auth_user_permission as $permission){
 					$user_group = array();
 					if($user['id'] == $permission['user_ref']){
-						array_push($user_group, $permission);
-						// d($user_group);
-						array_push($info_users[groups], $user_group[0]);
+
+						$queryFive = "SELECT * FROM auth_group, auth_permission WHERE auth_permission.id = '$permission[permission_ref]' AND auth_group.id = '$permission[group_ref]' ";
+						$info_group_permission = $sql->query($queryFive);
+						// d($info_group_permission[0]);
+						array_push($user_group, $info_group_permission[0]);
+
+						array_push($info_users[permission], $user_group[0]);
 					}
 				}
-				// array_push($user_list, $user_group);
 			}
 			array_push($user_list, $info_users);
 		}
-		d($user_list);
+		// d($user_list);
 		
 		#created group user
 		$id_user_create_group = $group_info[0]['created_by'];
@@ -65,10 +65,15 @@ if(!$auth->is_logged()){
 		$info_by_create = $sql->query($querySix);
 		$create_by = $info_by_create[0]['first_name'] .' '. $info_by_create[0]['last_name'];
 
+		#info permissions table
+		$querySeven = "SELECT * FROM auth_permission";
+		$table_permission = $sql->query($querySeven);
+		
 		#sending data to view
 		$items_to_print = array();
 		$items_to_print['group'] = $group_info[0];
 		$items_to_print['users'] = $user_list;
+		$items_to_print['table_permission'] = $table_permission;
 		$items_to_print['create_by'] = $create_by;
 		\xeki\html_manager::render('dashboard/group_view.html',$items_to_print);
 	}else{
