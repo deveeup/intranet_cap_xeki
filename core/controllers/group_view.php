68d1bf561eed220fc35885563dcf8c6e661007fe
David R <jossedaviid1@gmail.com>
2019-04-29 12:20:40 -0500

    fixed first step

--- core/controllers/group_view.php
+++ core/controllers/group_view.php
@@ -24,41 +24,44 @@ if(!$auth->is_logged()){
 		#info group
 		$queryOne = "SELECT * FROM auth_group WHERE id = '$id_group'";
 		$group_info = $sql->query($queryOne);
-
-		#relationship user & group
+		
+		#relationship user & group (users in group)
 		$queryTwo = "SELECT * FROM auth_user_group WHERE group_ref = '$id_group'";
 		$group_user = $sql->query($queryTwo);
 
 		#send data users
 		$user_list = array();
 		$user_group = array();
-;		foreach ($group_user as $group){
-
+		foreach ($group_user as $group){
+			#list users (complete info)
 			$queryThree = "SELECT * FROM auth_user WHERE auth_user.id = '$group[user_ref]' ";
 			$array_user = $sql->query($queryThree);
+			$info_users = $array_user[0];
+			// d($info_users);
 
-			$queryFive = "SELECT * FROM auth_user_permission WHERE auth_user_permission.user_ref = '$group[user_ref]' ";
-			$array_user_permission = $sql->query($queryFive);
-
-			foreach ($array_user_permission as $id_group ){
-				// array_push($array_user[0], $array_user_permission[0]);
-				$groupRef = $id_group['group_ref'];
-				$querySix = "SELECT * FROM auth_group WHERE id = '$groupRef' ";
-				$infoGroupUser = $sql->query($querySix);
-				array_push($user_group, $infoGroupUser);
+			foreach ($array_user as $user){
+				$queryFour = "SELECT * FROM auth_user_permission WHERE auth_user_permission.user_ref = '$user[id]'  ";
+				$response_auth_user_permission = $sql->query($queryFour);
+				// d($response_auth_user_permission);
+				foreach ($response_auth_user_permission as $permission){
+					array_push($user_group, $permission);
+				}
+				if($user['id'] == $permission['user_ref']){
+					// d($user_group);
+					array_push($info_users, $user_group);
+				}
+				// array_push($user_list, $user_group);
 			}
-
-			array_push($user_list, $array_user[0]);
+			array_push($user_list, $info_users);
 		}
 		d($user_list);
 
+		
 		#created group user
 		$id_user_create_group = $group_info[0]['created_by'];
-		$queryFour = "SELECT * FROM auth_user WHERE id = '$id_user_create_group'";
-		$info_by_create = $sql->query($queryFour);
+		$querySix = "SELECT * FROM auth_user WHERE id = '$id_user_create_group'";
+		$info_by_create = $sql->query($querySix);
 		$create_by = $info_by_create[0]['first_name'] .' '. $info_by_create[0]['last_name'];
-
-		#
 
 		#sending data to view
 		$items_to_print = array();
