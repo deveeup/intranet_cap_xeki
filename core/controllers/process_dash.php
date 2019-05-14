596cf23253b5aba7578eb8101d33bf98d36e50ae
David R <jossedaviid1@gmail.com>
2019-05-14 11:37:12 -0500

    select info data

--- core/controllers/process_dash.php
+++ core/controllers/process_dash.php
@@ -18,30 +18,23 @@ if(!$auth->is_logged()){
 	$queryOne = "SELECT * FROM transversal_process";
 	$transversal_process = $sql->query($queryOne);
 	
-	$item_process = array();
-	foreach ($transversal_process as $process){
+	$items_process = array();
 
+	foreach ($transversal_process as $process){
 		$queryTwo = "SELECT * FROM transversal_process_item WHERE transversal_process_item.father_id = '$process[id]' ";
 		$item_process = $sql->query($queryTwo);
-
 		$process[items] = array();
-
 		foreach ($item_process as $item){
-			if($process['id'] == $item['father_id']){
-				array_push($process[items], $item);
-			}
+			// if($process['id'] == $item['father_id']){
+			// 	array_push($process[items], $item);
+			// }
+			array_push($items_process, $item);
 		}
-		d($process);
 	}
-	
-	// $queryTwo = "SELECT * FROM transversal_process_item WHERE father_id = $id ORDER BY order_by ASC";
-	// $transversal_process_item = $sql->query($queryTwo);
-	// d($transversal_process_item);
-
-
 	
 	#sending data to view
 	$items_to_print=array();
 	$items_to_print['transversal_process'] = $transversal_process;
+	$items_to_print['items'] = $items_process;
 	\xeki\html_manager::render('dashboard/process.html', $items_to_print);
 }
