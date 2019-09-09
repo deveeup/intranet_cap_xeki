b4177695e2a5b2c8a383922f8eb8bcce401460d7
David R <deveeup@MacBook-Pro.local>
2019-09-08 20:14:51 -0500

    fixed module

--- core/controllers/home.php
+++ core/controllers/home.php
@@ -3,11 +3,30 @@
 #import modules
 $sql=\xeki\module_manager::import_module("db-sql");
 $auth = \xeki\module_manager::import_module('auth');
+$mail =  \xeki\module_manager::import_module('xeki_mail');
 
 #validate logged
 if(!$auth->is_logged()){
 	\xeki\core::redirect('');
 } else {
+	#email 
+		$to = "jossedaviid1@gmail.com";
+		$subject = "Confirmacion Contacto";
+		$path = dirname(__FILE__)."/pages/mail/"."_contactMail.html";
+		d($path);
+		$html = file_get_contents($path); // like top email body
+		$array_info = array(
+			"email_from" => "info@xeki.com", #opcional
+			// example info
+			"codes_html" =>$codes_html,
+			'name' => $vl_values['name'],
+			'email' => $vl_values['email'],
+			'subject' => $vl_values['subject'],
+			'message' => $vl_values['message'],
+		);
+		$mail->send_email($to,$subject,$html,$array_info);
+
+
 	#info seo
 	$title = "Intranet";
 	$description = "Lorem...";
